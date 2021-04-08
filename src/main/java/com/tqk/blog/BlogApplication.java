13cef6ddc4d6507362313914259aead91bce7bbf
tianqikai <953811508@qq.com>
2021-04-08 18:47:26 +0800

    修改友情链接等等

--- src/main/java/com/tqk/blog/BlogApplication.java
+++ src/main/java/com/tqk/blog/BlogApplication.java
@@ -2,6 +2,8 @@ package com.tqk.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -13,8 +15,8 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @EnableTransactionManagement
 @MapperScan("com.tqk.blog.mapper")
-@SpringBootApplication
-//@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+//@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
